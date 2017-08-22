e1594fe721337ef8d2d1ed8ebb68dedd1cf4585f
goliveiragabriel <goliveiragabriel@gmail.com>
2017-08-22 20:03:17 -0300

    Rename Project

---
+++ src/Garapa.Domain/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Garapa.Domain.Core.Models;
+
+namespace Garapa.Domain.Models
+{
+    public class User : Entity
+    {
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        protected User() { }
+    }
+}
---
+++ src/Garapa.Infra.Data/Context/GarapaContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cantina.Infra.Data.Context
+{
+    class GarapaContext
+    {
+    }
+}
